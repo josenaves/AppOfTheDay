821f382a626111d089a9bc759f423d575e2e3a1e
José Naves <josenaves@gmail.com>
2018-07-04 10:39:22 -0300

    Part 2 - animation

--- App.js
+++ App.js
@@ -21,15 +21,70 @@ var images = [
   { id: 4, src: require('./assets/4.jpg') },
 ]
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      activeImage: null
+    }
+  }
+  componentWillMount() {
+    this.allImages = {};
+    this.oldPosition = {};
+    this.position = new Animated.ValueXY();
+    this.dimensions = new Animated.ValueXY();
+  }
+
+  openImage = (index) => {
+    this.allImages[index].measure((x, y, width, height, pageX, pageY) => {
+      this.oldPosition.x = pageX;
+      this.oldPosition.y = pageY;
+      this.oldPosition.width = width;
+      this.oldPosition.height = height;
+
+      this.position.setValue({
+        x: pageX,
+        y: pageY
+      });
+
+      this.dimensions.setValue({
+        x: width,
+        y: height
+      });
+
+      this.setState({
+        activeImage: images[index]
+      }, () => {
+        this.viewImage.measure((dx, dy, dWidth, dHeight, dPageX, dPageY) => {
+          Animated.parallel([
+            Animated.timing(this.position.x, { toValue: dPageX, duration: 300 }),
+            Animated.timing(this.position.y, { toValue: dPageY, duration: 300 }),
+            Animated.timing(this.dimensions.x, { toValue: dWidth, duration: 300 }),
+            Animated.timing(this.dimensions.y, { toValue: dHeight, duration: 300 })
+          ]).start()
+        })
+      });
+    });
+  }
+
   render() {
+    const activeImageStyle = {
+      width: this.dimensions.x,
+      height: this.dimensions.y,
+      left: this.position.x,
+      top: this.position.y
+    };
+
     return (
       <SafeAreaView style={{ flex: 1 }}>
         <ScrollView style={{ flex: 1 }}>
-        { images.map(image => {
+        { images.map((image, index) => {
           return (
-            <TouchableWithoutFeedback key={image.id}>
+            <TouchableWithoutFeedback
+              onPress={() => this.openImage(index)}
+              key={image.id}>
               <Animated.View style={{ height: SCREEN_HEIGHT - 150, width: SCREEN_WIDTH, padding: 15 }}>
                 <Image
+                  ref={(image) => (this.allImages[index] = image)}
                   source={ image.src }
                   style={{ flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 20 }
                 }/>
@@ -38,6 +93,19 @@ export default class App extends React.Component {
           )
         })}
         </ScrollView>
+
+        <View style={StyleSheet.absoluteFill}
+          pointerEvents={this.state.activeImage ? "auto" : "none"}
+        >
+          <View style={{ flex: 2, borderWidth: 1 }} ref={(view) => (this.viewImage = view)}>
+            <Animated.Image
+              source={this.state.activeImage ? this.state.activeImage.src : null}
+              style={[{ resizeMode: 'cover', top: 0, left: 0, height: null }, activeImageStyle]}
+            >
+            </Animated.Image>
+
+          </View>
+        </View>
       </SafeAreaView>
     );
   }
